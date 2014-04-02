ef003f936e11de62ba04617195f76ed3fe0a6b8d
d-smith <doug.smith.mail@gmail.com>
2014-04-02 20:22:52 +0000

    Added private subnet and security group defs.

--- create_vpc.rb
+++ create_vpc.rb
@@ -47,3 +47,54 @@ rtAssociation = ec2Client.associate_route_table({
 
 puts "Associated route to subnet - #{rtAssociation}"
 
+# Create a private subnet
+privateSubnet = ec2Client.create_subnet({
+	:vpc_id => vpc[:vpc_id],
+	:cidr_block => "10.0.1.0/24",
+	:availability_zone => "us-east-1c"		
+})
+
+# Create a security group allowing inbound ssh from anywhere. This will be used in launching
+# EC2 instances in the public subnet
+launch_group_id = ec2Client.create_security_group({
+	:group_name => "launch-sg",
+	:description => "launch-sg",
+	:vpc_id => vpc[:vpc_id]
+})[:group_id]
+
+puts("created security group #{launch_group_id}")
+
+ec2Client.authorize_security_group_ingress({
+	:group_id => launch_group_id,
+	:ip_permissions => [
+		{:ip_protocol => "tcp",
+		 :from_port => 22,
+		 :to_port => 22,
+		 :ip_ranges => [
+		 	{:cidr_ip => "0.0.0.0/0"}
+		 ]
+		}
+	]
+})
+
+
+# Create a security group allowing access from the VPC. This will be used when launching private instances.
+private_launch_group_id = ec2Client.create_security_group({
+	:group_name => "private-subnet-launch-sg",
+	:description => "private-subnet-launch-sg",
+	:vpc_id => vpc[:vpc_id]	
+})[:group_id]
+
+ec2Client.authorize_security_group_ingress({
+	:group_id => private_launch_group_id,
+	:ip_permissions => [
+		{:ip_protocol => "tcp",
+		 :from_port => 22,
+		 :to_port => 22,
+		 :ip_ranges => [
+		 	{:cidr_ip => "10.0.0.0/16"}
+		 ]
+		}
+	]
+})
+

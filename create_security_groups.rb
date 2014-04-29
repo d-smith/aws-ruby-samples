9da154060ad7c9bc7e26e1b6b57cd7edd8777488
d-smith <doug.smith.mail@gmail.com>
2014-04-29 15:38:59 +0000

    Refactored security group creation

--- create_security_groups.rb
+++ create_security_groups.rb
@@ -1,5 +1,14 @@
 require File.expand_path(File.dirname(__FILE__) + '/config')
 
+def wait_for_group_creation(sg)
+  created = false
+  while !created do
+    created = sg.exists?
+  end
+end
+
+
+
 (vpc_id, dummyarg) = ARGV
 unless vpc_id
   puts "Usage: create_security_groups <VPC_ID>"
@@ -19,6 +28,8 @@ launch_sg = security_groups.create("launch-sg", {
     :vpc => vpc
 })
 
+wait_for_group_creation(launch_sg)
+
 launch_sg.authorize_ingress(:tcp, 22, "0.0.0.0/0")
 
 # Create the security group used to launch instances in the private subnets.
@@ -28,5 +39,29 @@ private_launch_sg = security_groups.create("private-subnet-launch-sg", {
     :vpc => vpc
 })
 
+wait_for_group_creation(private_launch_sg)
+
 private_launch_sg.authorize_ingress(:tcp, 22, launch_sg)
 private_launch_sg.authorize_ingress(:tcp, 9000, launch_sg)
+
+# Create a database security group to allow SQL*Net traffic from
+# instances launched using the private-subnet-launch-sg
+sqlnet_sg = security_groups.create("sqlnet-sg", {
+    :description => "SQLNet access from VPC private subnets",
+    :vpc => vpc
+})
+
+wait_for_group_creation(sqlnet_sg)
+
+sqlnet_sg.authorize_ingress(:tcp, 1521, private_launch_sg)
+
+# Create the security group for the load balancer
+
+load_balancer_sg = security_groups.create("load_balancer_sg", {
+    :description => "Load balancer security group",
+    :vpc => vpc
+})
+
+wait_for_group_creation(load_balancer_sg)
+
+load_balancer_sg.authorize_ingress(:tcp, 80, "0.0.0.0/0")

2dfbf4a27d73838e794cdd3593d7fe853c112ac8
d-smith <doug.smith.mail@gmail.com>
2015-03-03 11:31:22 +0000

    Updated script to create all needed security groups

--- create_security_groups.rb
+++ create_security_groups.rb
@@ -27,6 +27,24 @@ def authorize_ingress(ec2, sg_id, port, cidr_ip)
   )
 end
 
+def authorize_ingress_from_sg(ec2, sg_id, port, traffic_src_sg)
+  ec2.authorize_security_group_ingress(
+    group_id: sg_id,
+    ip_permissions: [
+      {
+        ip_protocol: "tcp",
+        to_port: port,
+        from_port: port,
+        user_id_group_pairs:[
+          {
+            group_id: traffic_src_sg,
+          }
+        ]
+      },
+    ]
+  )
+end
+
 (vpc_id, dummyarg) = ARGV
 unless vpc_id
   puts "Usage: create_security_groups <VPC_ID>"
@@ -43,53 +61,32 @@ launch_sg = create_security_group(ec2, "launch-sg", vpc_id)
 puts "created sg #{launch_sg}"
 authorize_ingress(ec2, launch_sg, 22, "192.223.128.0/17")
 
-exit
-#TODO - resume port
 
-# Create the security group for the load balancer
-load_balancer_sg = security_groups.create("load_balancer_sg", {
-    :description => "Load balancer security group",
-    :vpc => vpc
-})
+load_balancer_sg = create_security_group(ec2, "load_balancer_sg", vpc_id)
+puts "created sg #{load_balancer_sg}"
+authorize_ingress(ec2, load_balancer_sg, 80, "0.0.0.0/0")
 
-wait_for_group_creation(load_balancer_sg)
-
-load_balancer_sg.authorize_ingress(:tcp, 80, "0.0.0.0/0")
 
 # Create the security group used to launch instances in the private subnets.
 # Authorize ingress from anything launched using the launch_sg
-private_launch_sg = security_groups.create("private-subnet-launch-sg", {
-    :description => "private-subnet-launch-sg",
-    :vpc => vpc
-})
+private_launch_sg = create_security_group(ec2, "private-subnet-launch-sg",vpc_id)
+puts "created sg #{private_launch_sg}"
 
-wait_for_group_creation(private_launch_sg)
 
-private_launch_sg.authorize_ingress(:tcp, 22, launch_sg)
-private_launch_sg.authorize_ingress(:tcp, 9000, launch_sg)
-private_launch_sg.authorize_ingress(:tcp, 9000, load_balancer_sg)
+authorize_ingress_from_sg(ec2, private_launch_sg, 22, launch_sg)
+authorize_ingress_from_sg(ec2, private_launch_sg, 9000, launch_sg)
+authorize_ingress_from_sg(ec2, private_launch_sg, 9000, load_balancer_sg)
+
 
 # Create a database security group to allow SQL*Net traffic from
 # instances launched using the private-subnet-launch-sg
-sqlnet_sg = security_groups.create("sqlnet-sg", {
-    :description => "SQLNet access from VPC private subnets",
-    :vpc => vpc
-})
-
-wait_for_group_creation(sqlnet_sg)
-
-sqlnet_sg.authorize_ingress(:tcp, 1521, private_launch_sg)
+sqlnet_sg = create_security_group(ec2, "sqlnet-sg", vpc_id)
+puts "created sg #{sqlnet_sg}"
+authorize_ingress_from_sg(ec2, sqlnet_sg, 1521, private_launch_sg)
 
 
-
-# Create a security group for the NAT
-nat_sg = security_groups.create("nat_sg", {
-  :description => "NAT instance security group",
-  :vpc => vpc
-})
-
-wait_for_group_creation(nat_sg)
-
-nat_sg.authorize_ingress(:tcp, 80, private_launch_sg)
-nat_sg.authorize_ingress(:tcp, 443, private_launch_sg)
-nat_sg.authorize_ingress(:tcp, 22, launch_sg)
+nat_sg = create_security_group(ec2, "sg_nat", vpc_id)
+puts "created #{nat_sg}"
+authorize_ingress_from_sg(ec2, nat_sg, 80, private_launch_sg)
+authorize_ingress_from_sg(ec2, nat_sg, 443, private_launch_sg)
+authorize_ingress_from_sg(ec2, nat_sg, 22, launch_sg)
